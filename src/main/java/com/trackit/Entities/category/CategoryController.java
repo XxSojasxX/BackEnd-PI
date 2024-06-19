9e9e7072ae76fc40857d5d16a8b65eb1e79b4376
Oslowxscrx <oer.nogales@yavirac.edu.ec>
2024-06-19 00:48:25 -0500

    crosOrigin

--- src/main/java/com/trackit/Entities/category/CategoryController.java
+++ src/main/java/com/trackit/Entities/category/CategoryController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.persistence.EntityNotFoundException;
 
 @RestController
+@CrossOrigin({"*"})
 @RequestMapping("/trackit/bodega/categories")
 @Tag(name = "Controlador de Categorias")
 public class CategoryController {
